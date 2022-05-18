17654658d8afb86180ea15bff0da1d037c361310
DrCash-KMark <markmission10@gmail.com>
2022-05-18 17:56:05 +0200

    Create PlayRandom.py
    Added FullRandom for finding solution

---
+++ PlayRandom.py
@@ -0,0 +1,45 @@
+from random import choice
+
+# this is imported for testing
+import boardGeneration
+
+# this file contain the logic behind the pure random selector
+Rows, Columns = (0, 0)
+Matrix = [[0 for x in range(Columns)] for y in range(Rows)]
+possibleOptions = []
+
+# TODO fix because this doesn't initialize the file as a class
+def initRandomPlayer(matrix, columns, rows):
+    global Rows, Columns, Matrix
+    Rows = rows
+    Columns = columns
+    Matrix = matrix
+
+
+def findPossibleOptions():
+    global possibleOptions, Rows, Columns, Matrix
+    for i in range(Rows - 1, -1, -1):
+        for j in range(0, Columns):
+            if Matrix[i][j] != 0:
+                possibleOptions.append(str(i) + ";" + str(j))
+
+
+# return the cordinates of a selected tile
+# format rowsIndex ; columIndex #TODO look into this statment
+def selecTile():
+    global possibleOptions
+    chosen = choice(possibleOptions)
+    return chosen
+
+
+def main():
+    Matrix = boardGeneration.generateBoardFullRandom(
+        columns=5, rows=6, numOfDifferentTiles=5
+    )
+    initRandomPlayer(Matrix, columns=5, rows=6)
+    findPossibleOptions()
+    selecTile()
+    print(selecTile())
+
+
+main()
