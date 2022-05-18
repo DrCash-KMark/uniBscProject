970392cb3864b75eb3881a165f3f82fe6b6fce0f
DrCash-KMark <markmission10@gmail.com>
2022-05-18 17:55:25 +0200

    fixed some files
    Added board generation (random)
    Little bit of documentation
    fixed some formating issues
    score calculation and main game loop

--- board.py
+++ board.py
@@ -1,93 +1,129 @@
 import os
 import sys
 
-#global values:
-columns, rows = (5,6)
-#left down is zero zero in the matrix
+# global values:
+columns, rows = (5, 6)
+# left down is zero zero in the matrix
 
 
-#never call this function with number being 0 it leads to an infinte recursion
-#indexing starts from 0
-def tileChosed(x,y,matrix,number):
-    numOfTilesHit=0
-    if matrix[x][y]==number:
-        matrix[x][y]=0
-        numOfTilesHit+=1
-        if y<rows-1:
-            numOfTilesHit += tileChosed(x,y+1,matrix,number)
-        if x<columns-1:
-            numOfTilesHit +=tileChosed(x+1,y,matrix,number)
-        if y>0:
-            numOfTilesHit +=tileChosed(x,y-1,matrix,number)
-        if x>0:
-            numOfTilesHit +=tileChosed(x-1,y,matrix,number)
+# never call this function with number being 0 it leads to an infinte recursion
+# indexing starts from 0
+def tileChosed(x, y, matrix, number):
+    numOfTilesHit = 0
+    if matrix[x][y] == number:
+        matrix[x][y] = 0
+        numOfTilesHit += 1
+        if y < rows - 1:
+            numOfTilesHit += tileChosed(x, y + 1, matrix, number)
+        if x < columns - 1:
+            numOfTilesHit += tileChosed(x + 1, y, matrix, number)
+        if y > 0:
+            numOfTilesHit += tileChosed(x, y - 1, matrix, number)
+        if x > 0:
+            numOfTilesHit += tileChosed(x - 1, y, matrix, number)
     return numOfTilesHit
-        
-def collapse(matrix):
-    #collapse the colums downwards
-    newPlaceId=-1
-    for w in range(0,columns):
-       for h in range(0,rows):
-           if matrix[h][w]== 0 and newPlaceId==-1: #initalizing where to collapse the colum downwards
-               newPlaceId=h
-           if matrix[h][w]!=0 and newPlaceId!=-1:
-               matrix[newPlaceId][w]=matrix[h][w] #collapsing the Tiles downwards
-               newPlaceId+=1
-               matrix[h][w]=0
-       newPlaceId=-1
-    
-    #collapsing the colums leftwards
-    newPlaceId=-1
-    for w in range(0, columns):#columns-1):
-        if matrix[0][w]==0 and newPlaceId==-1:
-            newPlaceId=w
-        if matrix[0][w]!=0 and newPlaceId!=-1:
-            for h in range(0,rows):
-                matrix[h][newPlaceId]=matrix[h][w]
-                matrix[h][w]=0
-            newPlaceId+=1
 
-#this prints the matrix as a player wouuld see it
+
+def collapse(matrix):
+    # collapse the colums downwards
+    newPlaceId = -1
+    for w in range(0, columns):
+        for h in range(0, rows):
+            if (
+                matrix[h][w] == 0 and newPlaceId == -1
+            ):  # initalizing where to collapse the colum downwards
+                newPlaceId = h
+            if matrix[h][w] != 0 and newPlaceId != -1:
+                matrix[newPlaceId][w] = matrix[h][w]  # collapsing the Tiles downwards
+                newPlaceId += 1
+                matrix[h][w] = 0
+        newPlaceId = -1
+
+    # collapsing the colums leftwards
+    newPlaceId = -1
+    for w in range(0, columns):  # columns-1):
+        if matrix[0][w] == 0 and newPlaceId == -1:
+            newPlaceId = w
+        if matrix[0][w] != 0 and newPlaceId != -1:
+            for h in range(0, rows):
+                matrix[h][newPlaceId] = matrix[h][w]
+                matrix[h][w] = 0
+            newPlaceId += 1
+
+
+# this prints the matrix as a player wouuld see it
 def printMatrixNice(matrix):
     print(str(columns) + ";" + str(rows))
-    for i in range(rows-1,-1,-1):
-        for j in range(0,columns):
+    for i in range(rows - 1, -1, -1):
+        for j in range(0, columns):
             print(matrix[i][j], end=" ")
         print()
     print()
 
-def initBoardFromFile(fileName):    
-    f = open(os.path.join(sys.path[0], fileName), "r") 
 
-    #f = open(pathToFile,'r')
+def initBoardFromFile(fileName):
+    f = open(os.path.join(sys.path[0], fileName), "r")
+
+    # f = open(pathToFile,'r')
     firstLine = f.readline().split(";")
     width = int(firstLine[0])
     height = int(firstLine[1])
-    #this reads in the board from a file which looks as a player would see
+    # this reads in the board from a file which looks as a player would see
     matrix = [[0 for x in range(width)] for y in range(height)]
-    for i in range(height-1,-1,-1):
+    for i in range(height - 1, -1, -1):
         stringLine = f.readline().split(";")
-        for j in range(0,width,1):
+        for j in range(0, width, 1):
             matrix[i][j] = int(stringLine[j])
     return matrix
 
+
+# Return the score achived with the removal of numberOfTitlesHit
+def calculateScore(numberOfTilesHit):
+    return (numberOfTilesHit - 1) ^ 2
+
+
+# The main loop of the game
+def gameLoop(matrix):
+    gameIsOn = True
+    totalScore = 0
+    printMatrixNice(matrix)
+    while gameIsOn:
+        # give the coordinates in a format of x;y for example: 1;2
+        inputData = input()
+        splitedData = inputData.split(";")
+        xCoordinate = splitedData[0]
+        yCoordinate = splitedData[1]
+        #
+        selectedTile = matrix[xCoordinate][yCoordinate]
+        # testing if the selected tile can be chosen
+        if selectedTile != 0:
+            titleHit = tileChosed(xCoordinate, yCoordinate, matrix, selectedTile)
+            collapse(matrix)
+            score = calculateScore(titleHit)
+            print(score)
+            totalScore += score
+        # since the collapse always moves item towards 0,0 if it is empty it means the game has ended because no mroe tiles remain.
+        if matrix[0][0] == 0:
+            gameIsOn = False
+
+
 def main():
     print("Hello World!")
-    
-    #init the table for the game
-    
-    Matrix = [[0 for i in range(columns)]for j in range(rows)]
+
+    # init the table for the game
+
+    Matrix = [[0 for i in range(columns)] for j in range(rows)]
     Matrix = initBoardFromFile("Map5x6.txt")
     printMatrixNice(Matrix)
-    #Matrix = [[0 for x in range(0,columns)] for y in range(0,rows)]
+    # Matrix = [[0 for x in range(0,columns)] for y in range(0,rows)]
     collapse(Matrix)
     printMatrixNice(Matrix)
-    tileHit=0
-    tileHit=tileChosed(2,1,Matrix,Matrix[2][1])
+    tileHit = 0
+    tileHit = tileChosed(2, 1, Matrix, Matrix[2][1])
     printMatrixNice(Matrix)
     print(tileHit)
     collapse(Matrix)
     printMatrixNice(Matrix)
-    
-    
+
+
 main()
--- boardGeneration.py
+++ boardGeneration.py
@@ -1,12 +1,49 @@
+from mimetypes import init
 import os
-from random import random
+import sys
+import random
+
+# Global parameters of the generated baord
+columns, rows = (5, 6)
+
+# initilazes an empty Matrix/board with the 2 given parameter
+def initEmptyMatrix(width, height):
+    # width=coluumns height=rows
+    matrix = [[0 for x in range(width)] for y in range(height)]
 
 
+# Generates a borad with no empty field.
+# you can set how diverse map you want.
+def generateBoardFullRandom(columns, rows, numOfDifferentTiles):
+    matrix = [[0 for x in range(columns)] for y in range(rows)]
+    # filling out the board
+    for i in range(rows - 1, -1, -1):
+        for j in range(0, columns):
+            matrix[i][j] = random.randrange(1, numOfDifferentTiles)
+    return matrix
 
-def generateBoardFullRandom(matrix, columns, rows, numOfDifferentTiles):
-    for i in range(rows-1,-1,-1):
-        for j in range(0,columns):
-            matrix[j][i]= random(1,numOfDifferentTiles)
+
+def writeBoardToFile(matrix, columns, rows, fileName):
+    f = open(os.path.join(sys.path[0], fileName), "w")
+    # writing out the data of the board
+    f.write(str(columns) + ";" + str(rows) + "\n")
+    # writing out the board
+    for i in range(rows - 1, -1, -1):
+        for j in range(0, columns):
+            f.write(str(matrix[i][j]))
+            if j != columns - 1:
+                f.write(";")
+        if i != 0:
+            f.write("\n")
+    #
+    f.close
+
 
 def main():
-    generateBoardFullRandom()
+    Matrix = initEmptyMatrix(columns, rows)
+    Matrix = generateBoardFullRandom(columns, rows, numOfDifferentTiles=5)
+    writeBoardToFile(Matrix, columns, rows, "Test01.csv")
+    print(Matrix)
+
+
+# main()
